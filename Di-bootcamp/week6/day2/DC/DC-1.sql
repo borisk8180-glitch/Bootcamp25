70a005d4a47ed9abd911a41fef01bfb016bebc3b
borisk8180-glitch <borisk8180@gmail.com>
2025-09-30 18:01:34 +0300

    day2

--- Di-bootcamp/week6/day2/DC/DC-1.sql
+++ Di-bootcamp/week6/day2/DC/DC-1.sql
@@ -21,15 +21,16 @@ INSERT INTO SecondTab VALUES
 
 SELECT * FROM SecondTab
 
+--Q1 count = 0
 SELECT COUNT(*) 
     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
-
+--Q2 count = 2
 SELECT COUNT(*) 
     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
-
+--Q3 Count = 0
 SELECT COUNT(*) 
     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
-
+--Q4 Count = 2
 SELECT COUNT(*) 
     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
 	
